e144d3e1fe9b89274c6033aa71be5deeee4fa133
mason2woods <32215289+mason2woods@users.noreply.github.com>
2021-04-20 13:00:05 +0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -4,5 +4,5 @@ document.write ("<div class='footer'>");
         document.write ("<a href='https://docs.qq.com/form/page/DV1loR3N4UFhtSmhE?_w_tencentdocx_form=1' target='_blank'>网站留言</a> 丨 <a href='https://github.com/mason2woods/home' target='_blank'>Github</a>");
     document.write ("</div>");
     // document.write ("<p class='statement'>声明：自己专用的导航链接，不分享！</p>");
-    document.write ("<p class='copyright'>Copyright 2019-2020 © Woods. All Rights Reserved. </p>");
+    document.write ("<p class='copyright'>Copyright 2019-2021 © Woods. All Rights Reserved. </p>");
 document.write ("</div>")
